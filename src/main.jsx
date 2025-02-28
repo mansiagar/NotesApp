15f022dc282b6ff9db5b9b57c143a6ab42bd9cb1
Mansi agarwal <mansiagarwal293@gmail.com>
2025-02-28 12:23:35 +0530

    hash router added

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,17 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { Provider } from "react-redux";
 import { Provider as ChakraProvider } from "./components/ui/provider";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { store } from "./redux/store";
 
 import Routing from "./Route";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <ChakraProvider>
         <Routing />
       </ChakraProvider>
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 );
