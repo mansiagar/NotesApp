c18d87473c414aa4a520220fe3cb67df37fc152a
Mansi agarwal <mansiagarwal293@gmail.com>
2025-03-01 11:27:54 +0530

    auth completed

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,12 +3,12 @@ import {
   combineReducers,
   legacy_createStore as createstore,
 } from "redux";
-import { notesReduer } from "./notesReducer";
+//import { notesReduer } from "./notesReducer";
 import { thunk } from "redux-thunk";
 import { authReducer } from "./authReducer";
 
 const rootReducer = combineReducers({
   auth: authReducer,
-  notes: notesReduer,
+  // notes: notesReduer,
 });
 export const store = createstore(rootReducer, applyMiddleware(thunk));
